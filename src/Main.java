5debb52dc1a0e8a0ec661fba8bea2838baed6581
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-10-29 19:33:17 +0400

    Дз.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import jdk.jfr.Category;
 
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -96,10 +96,89 @@ public class Main {
                 tourismo1
         );
 
+        ServiceStation serviceStation = new ServiceStation();
+        serviceStation.addCar(ladaGranta);
+        serviceStation.addTrucks(ural);
+        serviceStation.service();
+        serviceStation.service();
+
         for (Transport transport: transports) {
             printInfo(transport);
         }
+
+        System.out.println();
+
+        Queue<String> queue1 = new ArrayDeque<>();
+        Queue<String> queue2 = new ArrayDeque<>();
+        randomFilling(queue1);
+        randomFilling(queue2);
+        System.out.println("Первая очередь: " + queue1);
+        System.out.println("Вторая очередь: " + queue2);
+
+        add("Владимир Петров", queue1, queue2);
+        System.out.println();
+
+        System.out.println("Первая очередь: " + queue1);
+        System.out.println("Вторая очередь: " + queue2);
+        System.out.println();
+
+        example();
     }
+
+    private static void example() {
+        List<List<String>> biDemArrList = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            biDemArrList.add(i, new ArrayList<>());
+            for (int j = 0; j < 8; j++) {
+                biDemArrList.get(i).add(j, ((i+j)%2==1?"●":"◯"));
+            }
+        }
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(biDemArrList.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void add (String name, Queue<String> queue1, Queue<String> queue2) {
+        if (queue1.size() == MAX_SIZE && queue2.size() == MAX_SIZE) {
+            System.out.println("ГАЛЯ, я вызываю тебя!");
+            return;
+        }
+
+        if (queue1.size() < queue2.size()) {
+            queue1.offer(name);
+        } else {
+            queue2.offer(name);
+        }
+    }
+
+    private static void remove (Queue<String> queue1, Queue<String> queue2) {
+        if (RANDOM.nextBoolean()) {
+            queue1.poll();
+        } else {
+            queue2.poll();
+        }
+    }
+
+    private static void randomFilling(Queue<String> queue) {
+        int size = RANDOM.nextInt(MAX_SIZE + 1);
+        for (int i = 0; i < size; i++) {
+            queue.offer(NAMES.get(RANDOM.nextInt(NAMES.size())));
+        }
+    }
+
+    private static final List<String> NAMES = List.of(
+            "Иваныч",
+            "Петрович",
+            "Саныч",
+            "Кая",
+            "Парадеевич"
+    );
+
+    private static final Random RANDOM = new Random();
+    private static final int MAX_SIZE = 5;
 
     private static void printInfo(Transport transport) {
         System.out.println("Информация по автомобилю " + transport.getBrand() + " " + transport.getModel());
